ef774e12a9305c7f15aa15e34d8a1048dfeb21ec
ROCCO LUIGI PETRUZZI s280126 <s280126@studenti.polito.it>
2020-06-02 02:28:03 +0200

    test: GUI tests for UC1 to UC6

---
+++ docs/GUITests/testUC6_1.sikuli/testUC6_1.py
@@ -0,0 +1,9 @@
+click("1591036894868.png")
+type("1591048946658.png", "admin")
+type("1591048939028.png", "admin@ezgas.com")
+click("1591049894214.png")
+click("1591049923425.png")
+for n in range(30):
+    type(Key.DOWN)
+click("1591057152064.png")
+find("1591057200566.png")
