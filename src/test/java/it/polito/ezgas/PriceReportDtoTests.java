fb2ae59d8254a40db9fde66652fc25a60c9689ef
Marco Pappalardo s277867 <s277867@studenti.polito.it>
2020-06-26 01:34:22 +0200

    fix: improved whitebox coverage

--- src/test/java/it/polito/ezgas/PriceReportDtoTests.java
+++ src/test/java/it/polito/ezgas/PriceReportDtoTests.java
@@ -1,5 +1,8 @@
 package it.polito.ezgas;
 
+
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,19 +21,19 @@ public class PriceReportDtoTests {
 	@Test
 	public void TC1_PriceReportDtoGasStationID() {
 		pr.setGasStationId(1250);
-		assert (pr.getGasStationId() == 1250);
+		assertEquals(pr.getGasStationId() ,1250,0);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoGasStationID() {
 		pr.setGasStationId(Integer.MAX_VALUE + 1);
-		assert (pr.getGasStationId() == Integer.MIN_VALUE);
+		assertEquals (pr.getGasStationId() , Integer.MIN_VALUE,0);
 	}
 
 	@Test
 	public void TC3_PriceReportDtoGasStationID() {
 		pr.setGasStationId(Integer.MIN_VALUE - 1);
-		assert (pr.getGasStationId() == Integer.MAX_VALUE);
+		assertEquals (pr.getGasStationId() , Integer.MAX_VALUE,0);
 	}
 
 
@@ -38,133 +41,133 @@ public class PriceReportDtoTests {
 	public void TC1_PriceReportDtoDieselPrice() {
 		Double value = 12.45;
 		pr.setDieselPrice(value);
-		assert (pr.getDieselPrice() == value);
+		assertEquals (pr.getDieselPrice() , value);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoDieselPrice() {
 		pr.setDieselPrice(-Double.MAX_VALUE - 1);
-		assert (pr.getDieselPrice() == -Double.MAX_VALUE);
+		assertEquals (pr.getDieselPrice() , -Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC3_PriceReportDtoDieselPrice() {
 		pr.setDieselPrice(Double.MAX_VALUE + 1);
-		assert (pr.getDieselPrice() == Double.MAX_VALUE);
+		assertEquals (pr.getDieselPrice() , Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC1_PriceReportDtoSuperPrice() {
 		Double value = 12.45;
 		pr.setSuperPrice(value);
-		assert (pr.getSuperPrice() == value);
+		assertEquals (pr.getSuperPrice() , value);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoSuperPrice() {
 		pr.setSuperPrice(-Double.MAX_VALUE - 1);
-		assert (pr.getSuperPrice() == -Double.MAX_VALUE);
+		assertEquals (pr.getSuperPrice() , -Double.MAX_VALUE,0);
 
 	}
 
 	@Test
 	public void TC3_PriceReportDtoSuperPrice() {
 		pr.setSuperPrice(Double.MAX_VALUE + 1);
-		assert (pr.getSuperPrice() == Double.MAX_VALUE);
+		assertEquals (pr.getSuperPrice() , Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC1_PriceReportDtoSuperPlusPrice() {
 		Double value = 12.45;
 		pr.setSuperPlusPrice(value);
-		assert (pr.getSuperPlusPrice() == value);
+		assertEquals (pr.getSuperPlusPrice() , value);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoSuperPlusPrice() {
 		pr.setSuperPlusPrice(-Double.MAX_VALUE - 1);
-		assert (pr.getSuperPlusPrice() == -Double.MAX_VALUE);
+		assertEquals (pr.getSuperPlusPrice() , -Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC3_PriceReportDtoSuperPlusPrice() {
 		pr.setSuperPlusPrice(Double.MAX_VALUE + 1);
-		assert (pr.getSuperPlusPrice() == Double.MAX_VALUE);
+		assertEquals (pr.getSuperPlusPrice() , Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC1_PriceReportDtoGasPrice() {
 		Double value = 12.45;
 		pr.setGasPrice(value);
-		assert (pr.getGasPrice() == value);
+		assertEquals (pr.getGasPrice() , value);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoGasPrice() {
 		pr.setGasPrice(-Double.MAX_VALUE - 1);
-		assert (pr.getGasPrice() == -Double.MAX_VALUE);
+		assertEquals (pr.getGasPrice() , -Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC3_PriceReportDtoGasPrice() {
 		pr.setGasPrice(Double.MAX_VALUE + 1);
-		assert (pr.getGasPrice() == Double.MAX_VALUE);
+		assertEquals (pr.getGasPrice() , Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC1_PriceReportDtoMethanePrice() {
 		Double value = 12.45;
 		pr.setMethanePrice(value);
-		assert (pr.getMethanePrice() == value);
+		assertEquals (pr.getMethanePrice() , value,0);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoMethanPrice() {
 		pr.setMethanePrice(-Double.MAX_VALUE - 1);
-		assert (pr.getMethanePrice() == -Double.MAX_VALUE);
+		assertEquals (pr.getMethanePrice() , -Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC3_PriceReportDtoMethanPrice() {
 		pr.setMethanePrice(Double.MAX_VALUE + 1);
-		assert (pr.getMethanePrice() == Double.MAX_VALUE);
+		assertEquals (pr.getMethanePrice() , Double.MAX_VALUE,0);
 	}
 	@Test
 	public void TC1_PriceReportDtoPremiumDieselPrice() {
 		Double value = 12.45;
 		pr.setPremiumDieselPrice(value);
-		assert (pr.getPremiumDieselPrice() == value);
+		assertEquals (pr.getPremiumDieselPrice() , value,0);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoPremiumDieselPrice() {
 		pr.setPremiumDieselPrice(-Double.MAX_VALUE - 1);
-		assert (pr.getPremiumDieselPrice() == -Double.MAX_VALUE);
+		assertEquals (pr.getPremiumDieselPrice() , -Double.MAX_VALUE,0);
 	}
 
 	@Test
 	public void TC3_PriceReportDtoPremiumDieselPrice() {
 		pr.setPremiumDieselPrice(Double.MAX_VALUE + 1);
-		assert (pr.getPremiumDieselPrice() == Double.MAX_VALUE);
+		assertEquals (pr.getPremiumDieselPrice() , Double.MAX_VALUE,0);
 	}
 	
 	
 	@Test
 	public void TC1_PriceReportDtoUserId() {
 		pr.setUserId(1250);
-		assert (pr.getUserId() == 1250);
+		assertEquals (pr.getUserId() , 1250,0);
 	}
 
 	@Test
 	public void TC2_PriceReportDtoUserId() {
 		pr.setUserId(Integer.MAX_VALUE + 1);
-		assert (pr.getUserId() == Integer.MIN_VALUE);
+		assertEquals (pr.getUserId() , Integer.MIN_VALUE,0);
 	}
 
 	@Test
 	public void TC3_PriceReportDtoUserId() {
 		pr.setUserId(Integer.MIN_VALUE - 1);
-		assert (pr.getUserId() == Integer.MAX_VALUE);
+		assertEquals (pr.getUserId() , Integer.MAX_VALUE,0);
 	}
 
 	
